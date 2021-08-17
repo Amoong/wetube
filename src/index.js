ad3b7812bc6fc3f0782f2c018ff5a904bcf2460a
MuYoungJeong <moo1323@naver.com>
2021-08-17 21:55:52 +0900

    get method

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const hanldeHome = (req, res) => {
+  return res.send("I still love you");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+};
+
+app.get("/", hanldeHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😉`);
 
 app.listen(PORT, handleListening);

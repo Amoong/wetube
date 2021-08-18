959cee60401532855ad52eeb88a20d5e1fcdcce4
MuYoungJeong <moo1323@naver.com>
2021-08-18 20:56:08 +0900

    middleware

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ const PORT = 4000;
 
 const app = express();
 
-const hanldeHome = (req, res) => {
-  return res.send("I still love you");
+const wowMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
+
+const hanldeHome = (req, res, next) => {
+  return res.send("<h1> hi </h1>");
 };
 
 const handleLogin = (req, res) => {
-  return res.send("Login here");
+  return res.send({ message: "hi" });
 };
 
-app.get("/", hanldeHome);
+app.get("/", wowMiddleware, hanldeHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😉`);

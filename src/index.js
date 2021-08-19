8a70e7dd3a5761329853d17920c6876c4f2afabc
MuYoungJeong <moo1323@naver.com>
2021-08-19 21:12:04 +0900

    global middleware

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
-const wowMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1> Not Allowed</h1>");
+  } else {
+    console.log("Allowed! you may continue.");
+    next();
+  }
 };
 
 const hanldeHome = (req, res, next) => {
@@ -17,8 +26,15 @@ const handleLogin = (req, res) => {
   return res.send({ message: "hi" });
 };
 
-app.get("/", wowMiddleware, hanldeHome);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", hanldeHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😉`);
 

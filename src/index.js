9b41158ae3d3fe196680c76a371d7756a75d929e
MuYoungJeong <moo1323@naver.com>
2021-08-22 22:58:43 +0900

    Add Router

--- src/index.js
+++ src/index.js
@@ -6,17 +6,29 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("tiny");
 
-const hanldeHome = (req, res, next) => {
-  return res.send("<h1> hi </h1>");
-};
-
-const handleLogin = (req, res) => {
-  return res.send({ message: "hi" });
-};
-
 app.use(logger);
-app.get("/", hanldeHome);
-app.get("/login", handleLogin);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😉`);
 

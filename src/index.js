73823ffdd5432884185a4d16b04e2e33353c6b78
MuYoungJeong <moo1323@naver.com>
2021-08-22 22:33:13 +0900

    Add morgan

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1> Not Allowed</h1>");
-  } else {
-    console.log("Allowed! you may continue.");
-    next();
-  }
-};
+const logger = morgan("tiny");
 
 const hanldeHome = (req, res, next) => {
   return res.send("<h1> hi </h1>");
@@ -26,15 +14,9 @@ const handleLogin = (req, res) => {
   return res.send({ message: "hi" });
 };
 
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge");
-};
-
 app.use(logger);
-app.use(privateMiddleware);
 app.get("/", hanldeHome);
 app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😉`);
 

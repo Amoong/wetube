4b244cfd5ad011e73aaa67e08b75533713454acf
MuYoungJeong <moo1323@naver.com>
2021-09-20 14:22:27 +0900

    Upload video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -54,3 +54,22 @@ export const postEdit = (req, res) => {
   video.title = title;
   return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  // here we will add a video to the videos array
+  return res.redirect("/");
+};

72b968b55c08f600603dc679ae02b970826f99c2
MuYoungJeong <moo1323@naver.com>
2021-12-29 21:31:29 +0900

    Search part two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -73,12 +73,18 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
 
-  if (!keyword) {
-    res.redirect("/");
+  let videos = [];
+
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
   }
 
-  return res.render("search", { pageTitle: "Search" });
+  return res.render("search", { pageTitle: "Search", videos });
 };

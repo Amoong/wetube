dbc54f8caf823127cde66927527616ccb79a23f1
Amoong <moo1323@naver.com>
2022-07-09 19:55:22 +0900

    Edit Profile GET

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,7 @@
 export const localsMiddleware = (req, res, next) => {
+  console.log(res.locals);
   res.locals.loggedIn = req.session.loggedIn;
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
   next();
 };

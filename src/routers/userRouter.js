109c521b1d21bf979de3456f266b49f9ba676bc2
Amoong <moo1323@naver.com>
2022-07-20 22:11:02 +0900

    User Profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -29,6 +29,6 @@ userRouter
   .all(protectorMiddleware)
   .get(getChangePassword)
   .post(postChangePassword);
-userRouter.get(":id", see);
+userRouter.get("/:id", see);
 
 export default userRouter;

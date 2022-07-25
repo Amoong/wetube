6d6dfcd7eab08bc535e768f1c32d8a55a5d45585
Amoong <moo1323@naver.com>
2022-07-25 20:28:42 +0900

    Video Owner part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import Video from "../models/Video";
 import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
@@ -210,9 +211,12 @@ export const see = async (req, res) => {
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
+  const videos = await Video.find({ owner: user._id });
+  console.log(videos);
 
   return res.render("users/profile", {
     pageTitle: user.name,
     user,
+    videos,
   });
 };

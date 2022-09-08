9702ce3b0b3301fc2155df98452823b2bb2083da
Amoong <moo1323@naver.com>
2022-09-08 22:07:27 +0900

    Recording Video part Two

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,44 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+
+const handleDownload = () => {};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    // 브라우저 메모리에 파일을 올리고 해당 메모리에 접근 할 수 있는 URL을 제공한다.
+    const videoFile = URL.createObjectURL(e.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);

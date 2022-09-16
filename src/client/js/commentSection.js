5ac3526d0589d99eee2c3b2ee1971189d2340daa
Amoong <moo1323@naver.com>
2022-09-16 22:51:54 +0900

    Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,20 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (e) => {
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
+const handleSubmit = async (e) => {
   e.preventDefault();
   const textarea = form.querySelector("textarea");
   const videoId = videoContainer.dataset.id;
@@ -11,13 +24,17 @@ const handleSubmit = (e) => {
     return;
   }
 
-  fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
+
+  if (status === 201) {
+    addComment(text);
+  }
 
   textarea.value = "";
 };

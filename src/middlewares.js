2a3afbb944b867bd865f5a2885af8190459f9223
Amoong <moo1323@naver.com>
2022-09-20 20:44:01 +0900

    isHeroku

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,17 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
   s3,
-  bucket: "wetube-mu",
+  bucket: "wetube-mu/images",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3,
+  bucket: "wetube-mu/videos",
   acl: "public-read",
 });
 
@@ -19,6 +27,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = req.session.loggedIn;
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -45,11 +54,11 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
   dest: "uploads/videos",
   limits: { fileSize: 10000000 },
-  storage: multerUploader,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
